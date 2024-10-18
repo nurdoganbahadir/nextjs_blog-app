4261aa402f9c597120e61eec44a06423a5d01c3f
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-18 20:49:19 +0300

    login ve register sayfaları eklendi

---
+++ pages/auth/login.js
@@ -0,0 +1,43 @@
+import { Avatar, Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import LoginForm from "../../components/auth/LoginForm";
+
+const Login = () => {
+  return (
+    <Container maxWidth="lg" sx={{ minHeight: "76.7vh" }}>
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        alignItems="center"
+        sx={{
+          my: "5rem",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "primary.main",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <PersonIcon />
+          </Avatar>
+          <Typography variant="h4" align="center" mb={4} color="primary.main">
+            Login
+          </Typography>
+          <LoginForm />
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Box to="/register">Do you have not an account?</Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Login;
